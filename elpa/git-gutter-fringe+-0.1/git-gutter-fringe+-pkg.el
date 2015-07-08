994e122ed140fdbdbdccb497a5e0f2ccabccf564
Chris Howe-Jones <chris.howejones@gmail.com>
2015-07-08 10:21:11 +0100

    Update magit
    Also removed minor mode supression of auto-revert.REmoved git-gutter as
    it was conflicting.

---
+++ elpa/git-gutter-fringe+-0.1/git-gutter-fringe+-pkg.el
@@ -0,0 +1,1 @@
+(define-package "git-gutter-fringe+" "0.1" "Fringe version of git-gutter+.el" '((git-gutter+ "0.1") (fringe-helper "20130519.1641")) :url "https://github.com/nonsequitur/git-gutter-fringe-plus")
