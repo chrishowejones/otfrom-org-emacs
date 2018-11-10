08fbf9db3138079120b3591f776472f18b7b2831
Chris Howe-Jones <chris.howejones@gmail.com>
2018-11-10 12:14:29 +0000

    Add emojify & flycheck-joker linting.

---
+++ elpa/flycheck-joker-20180913.504/flycheck-joker-autoloads.el
@@ -0,0 +1,16 @@
+;;; flycheck-joker-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("flycheck-joker.el") (23526 51853 389789
+;;;;;;  358000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; flycheck-joker-autoloads.el ends here
