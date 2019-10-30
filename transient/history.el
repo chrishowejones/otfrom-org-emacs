30fac92bcfd5490f7b2ac706b2b9f4dbbc46d961
Chris Howe-Jones <chris.howejones@gmail.com>
2019-10-30 15:50:22 +0000

    Autothemer and some other tidy up.

--- transient/history.el
+++ transient/history.el
@@ -1,7 +1,10 @@
-((magit-branch nil)
+((magit-blame
+  ("-w"))
+ (magit-branch nil)
  (magit-commit nil)
  (magit-diff
-  ("--no-ext-diff" "--stat"))
+  ("--no-ext-diff" "--stat")
+  ("--no-ext-diff"))
  (magit-dispatch nil)
  (magit-fetch nil)
  (magit-log
@@ -17,6 +20,8 @@
  (magit-remote
   ("-f"))
  (magit-reset nil)
+ (magit-revert
+  ("--edit"))
  (magit-stash nil)
  (magit-tag nil
             ("--annotate")))
