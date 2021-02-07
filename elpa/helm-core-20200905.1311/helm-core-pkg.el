202e999ee8f2b741cb0b3168a4907ffef38eca59
Chris Howe-Jones <chris.howejones@gmail.com>
2021-02-07 11:28:33 +0000

    Updated packages

--- elpa/helm-core-20200905.1311/helm-core-pkg.el
+++
@@ -1,7 +0,0 @@
-(define-package "helm-core" "20200905.1311" "Development files for Helm"
-  '((emacs "25.1")
-    (async "1.9.4"))
-  :commit "e23039c485e85fa58f35dce0c9f740d235effe06" :url "https://emacs-helm.github.io/helm/")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
