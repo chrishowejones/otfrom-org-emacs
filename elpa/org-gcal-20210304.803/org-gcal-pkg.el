9def13367f47d970e22d09afcca5a0bdf1bf8574
Chris Howe-Jones <chris.howejones@gmail.com>
2021-03-22 19:35:39 +0000

    Updated packages

---
+++ elpa/org-gcal-20210304.803/org-gcal-pkg.el
@@ -0,0 +1,16 @@
+(define-package "org-gcal" "20210304.803" "Org sync with Google Calendar"
+  '((request "20190901")
+    (request-deferred "20181129")
+    (alert "0")
+    (persist "0")
+    (emacs "26"))
+  :commit "ff55b2117c074288633c35bc38f21db13b476bbd" :authors
+  '(("myuhe <yuhei.maeda_at_gmail.com>"))
+  :maintainer
+  '("Raimon Grau" . "raimonster@gmail.com")
+  :keywords
+  '("convenience")
+  :url "https://github.com/kidd/org-gcal.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
