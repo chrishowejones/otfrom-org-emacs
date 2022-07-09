4cf3e800e8c3ec4de740a398dda6d0859d04c3f6
Chris Howe-Jones <chris.howejones@gmail.com>
2022-07-09 09:02:29 +0100

    Updated packages
    Signed-off-by: Chris Howe-Jones <chris.howejones@gmail.com>

---
+++ elpa/gruvbox-theme-20220625.1642/gruvbox-theme-pkg.el
@@ -0,0 +1,10 @@
+(define-package "gruvbox-theme" "20220625.1642" "A retro-groove colour theme for Emacs"
+  '((autothemer "0.2"))
+  :commit "e88ee445b791b77078ab0be8aa972bfab3078ed0" :authors
+  '(("Jason Milkins" . "jasonm23@gmail.com"))
+  :maintainer
+  '("Jason Milkins" . "jasonm23@gmail.com")
+  :url "http://github.com/greduan/emacs-theme-gruvbox")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
