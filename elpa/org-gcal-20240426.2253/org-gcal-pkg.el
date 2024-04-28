f88b32aa15f4daea98f1fe5115d7ad81b79b6ecd
Chris Howe-Jones <chrishowe-jones@F124236Y7M.local>
2024-04-28 13:28:52 +0100

    More fixes for AND macbook

--- elpa/org-gcal-20240426.2253/org-gcal-pkg.el
+++ elpa/org-gcal-20240426.2253/org-gcal-pkg.el
@@ -1,4 +1,4 @@
-(define-package "org-gcal" "20221212.1817" "Org sync with Google Calendar"
+(define-package "org-gcal" "20240426.2253" "Org sync with Google Calendar"
   '((aio "1.0")
     (alert "1.2")
     (elnode "20190702.1509")
@@ -7,7 +7,7 @@
     (persist "0.4")
     (request "20190901")
     (request-deferred "20181129"))
-  :commit "9bb3720525ad1c45823abab8ce910dd1225e7dcd" :authors
+  :commit "9f9d93e4f0d5863b1318e9e702e1ee6e841c2649" :authors
   '(("myuhe <yuhei.maeda_at_gmail.com>"))
   :maintainer
   '("Raimon Grau" . "raimonster@gmail.com")
